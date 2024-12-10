2e1f49d9768e56e6e5f8695dde0d20be74a18192
Yuliya Bondarenko <juliyaodessa84@gmail.com>
2024-12-10 16:12:07 +0100

    2

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import Profile from "./Profile/Profile.jsx";
+import userData from "../assets/data/userData.json";
+import friends from "../assets/data/friends.json";
+import transactions from "../assets/data/transactions.json";
+import FriendList from "./FriendList/FriendList.jsx";
+import TransactionHistory from "./TransactionHistory/TransactionHistory.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+//FriendList;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "modern-normalize";
-import "./index.css";
-import App from "./App.jsx";
+import "./components/index.css";
+import App from "./components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
